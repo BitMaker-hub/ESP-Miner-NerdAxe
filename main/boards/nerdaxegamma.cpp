7c90193d1c32f1427be93b2398135eb21de123d5
BitMaker <bitmaker.me@proton.me>
2025-01-27 01:17:02 +0100

    fix max power
    fix maxPin Nerdaxe Gamma

--- main/boards/nerdaxegamma.cpp
+++ main/boards/nerdaxegamma.cpp
@@ -31,7 +31,7 @@ NerdaxeGamma::NerdaxeGamma() : NerdAxe() {
     m_fanPerc = 100;
     m_vr_maxTemp = TPS546_THROTTLE_TEMP; //Set max voltage regulator temp
 
-    m_maxPin = 20.0;
+    m_maxPin = 25.0;
     m_minPin = 5.0;
     m_maxVin = 5.5;
     m_minVin = 4.5;
